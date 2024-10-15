a5521caa5ab787717da721c5687752fbd3c41a08
saadshaikh0 <saad.shaikh0@gmail.com>
2024-10-15 23:21:59 +0530

    Added sw.js file

--- src/index.js
+++ src/index.js
@@ -4,3 +4,18 @@ import App from "./App";
 import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.log("ServiceWorker registration failed: ", error);
+      });
+  });
+}
